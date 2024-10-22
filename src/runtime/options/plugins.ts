0e3a54a0ae07a46e15a6b7a0f843dd5b66bc359e
Gábor Torma <gabor@torma.dev>
2024-10-22 16:02:43 +0200

    feat: use setPluginsDefaults for server

--- src/runtime/options/plugins.ts
+++ src/runtime/options/plugins.ts
@@ -1,4 +1,8 @@
+import type { Nuxt } from '@nuxt/schema'
 import type { Import } from 'unimport'
+import path from 'node:path'
+import { createResolver } from '@nuxt/kit'
+import { scanDirExports, scanExports } from 'unimport'
 
 export type PluginDir = string
 export type PluginDirs = Array<PluginDir>
@@ -9,4 +13,35 @@ export type Plugins = Array<Plugin>
 export interface PluginOptions {
   pluginDirs?: PluginDir | PluginDirs
   plugins?: Plugin | Plugins
+}
+
+const filterExports = ({ name, from, as }: Import) => name === 'default' || new RegExp(`^${as}\w{0,2}`).test(path.basename(from, path.extname(from)))
+
+export async function setPluginsDefaults(server: PluginOptions, nuxt: Nuxt, defaultDir: string) {
+  const resolver = createResolver(import.meta.url)
+
+  if (typeof server.pluginDirs === 'string' && server.pluginDirs)
+    server.pluginDirs = [server.pluginDirs]
+  if (!server.pluginDirs?.length)
+    server.pluginDirs = [defaultDir]
+
+  server.pluginDirs = (server.pluginDirs as PluginDirs).map(dir =>
+    resolver.resolve(nuxt.options.rootDir, dir),
+  )
+
+  const plugins: Plugins = [...(await scanDirExports(server.pluginDirs, {
+    filePatterns: ['*.ts'],
+    types: false,
+  })).filter(filterExports),
+  ]
+
+  if (!Array.isArray(server.plugins)) {
+    server.plugins = [server.plugins as Plugin]
+  }
+  for (const plugin of server.plugins) {
+    (typeof plugin === 'string')
+      ? plugins.push(...await scanExports(resolver.resolve(nuxt.options.rootDir, plugin), false))
+      : plugins.push(plugin)
+  }
+  server.plugins = plugins
 }
--- src/runtime/options/server.ts
+++ src/runtime/options/server.ts
@@ -1,12 +1,6 @@
 import type { Nuxt } from '@nuxt/schema'
-import type { Import } from 'unimport'
-import type { Plugin, PluginDirs, PluginOptions, Plugins } from './plugins'
-import path from 'node:path'
 import { createResolver } from '@nuxt/kit'
-import consola from 'consola'
-import { scanDirExports, scanExports } from 'unimport'
-
-const filterExports = ({ name, from, as }: Import) => name === 'default' || new RegExp(`^${as}\w{0,2}`).test(path.basename(from, path.extname(from)))
+import { type PluginOptions, setPluginsDefaults } from './plugins'
 
 export interface ServerOptions extends PluginOptions {}
 
@@ -17,29 +11,5 @@ export const serverDefaultOptions: ServerOptions = {
 
 export async function setServerDefaults(server: ServerOptions, nuxt: Nuxt) {
   const resolver = createResolver(import.meta.url)
-
-  if (typeof server.pluginDirs === 'string' && server.pluginDirs)
-    server.pluginDirs = [server.pluginDirs]
-  if (!server.pluginDirs?.length)
-    server.pluginDirs = [resolver.resolve(nuxt.options.serverDir, './feathers')]
-
-  server.pluginDirs = (server.pluginDirs as PluginDirs).map(dir =>
-    resolver.resolve(nuxt.options.rootDir, dir),
-  )
-
-  const plugins: Plugins = [...(await scanDirExports(server.pluginDirs, {
-    filePatterns: ['*.ts'],
-    types: false,
-  })).filter(filterExports),
-  ]
-
-  if (!Array.isArray(server.plugins)) {
-    server.plugins = [server.plugins as Plugin]
-  }
-  for (const plugin of server.plugins) {
-    (typeof plugin === 'string')
-      ? plugins.push(...await scanExports(resolver.resolve(nuxt.options.rootDir, plugin), false))
-      : plugins.push(plugin)
-  }
-  server.plugins = plugins
+  await setPluginsDefaults(server, nuxt, resolver.resolve(nuxt.options.serverDir, './feathers'))
 }
